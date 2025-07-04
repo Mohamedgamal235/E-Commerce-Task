b3cab4238d649895145dcaff217f4da1d2e325bd
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-07-05 02:02:42 +0300

    Add Project

---
+++ Simple-E-Commerce/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+    class Customer
+    {
+        public string Name { get; }
+        public double Balance { get; private set; }
+        public Cart Cart { get; } = new Cart();
+
+        public Customer(string name , double balance)
+        {
+            Name = name;
+            Balance = balance; 
+        }
+
+        public void ReduceBalance(double price)
+        {
+            Balance -= price;
+        }
+    }
+}
---
+++ Simple-E-Commerce/MobileScratchCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+    class MobileScratchCard : Product
+    {
+        public MobileScratchCard(string name , double price , int quantity) 
+            : base(name , price , quantity) { }
+    }
+}
