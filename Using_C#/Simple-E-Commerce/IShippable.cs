6ed7d9d23d73880dc502ac0682e48a95a9e858a3
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-07-05 14:39:57 +0300

    Delete Readme file

---
+++ Using_C#/Simple-E-Commerce/IShippable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+    interface IShippable
+    {
+        public double GetWeight();
+        public string GetName();
+    }
+}
---
+++ Using_C#/Simple-E-Commerce/TV.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+    class TV : Product , IShippable
+    {
+        public double Weight { get; }
+
+        public TV(string name , double price , int quantity , double weight)
+            :base(name , price , quantity)
+        {
+            Weight = weight; 
+        }
+
+        public string GetName()
+        {
+            return Name ;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    }
+}
