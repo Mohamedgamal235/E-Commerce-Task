3449e4077a6cf4cd5cf7b9133591a4786aca083c
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-07-05 14:37:42 +0300

    Add using C#

---
+++ UisngC#/Simple-E-Commerce/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+    class Product
+    {
+        public string Name { get; }
+        public double Price { get; }
+        public int Quantity { get; private set; }
+
+        public Product(string name , double price , int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+            
+        public void ReduceQuantity(int amount)
+        {
+            Quantity -= amount;
+        }
+
+    }
+}
